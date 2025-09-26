912407f576dd027b0adc958aa09e0b40c503aaa7
Reza Akmal <rezajakmal@gmail.com>
2025-09-26 15:14:07 +0700

    add dotenv for MongoDB connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,20 @@
 // import mongoose = require("mongoose");
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("MongoDB connected");
   } catch(error) {
-    console.log("error");
-    console.error(error);
+    console.error("Error connecting to MongoDB", error);
     process.exit(1);
   }
 }
+
+connectDB();
 
 export default connectDB;
